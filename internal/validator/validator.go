fcecea7bc5045f2a6a630baa16c57b22711690a7
Devaraja-Anu <anubusinesscc8@gmail.com>
2025-07-27 20:52:55 +0530

    feat: validator

---
+++ internal/validator/validator.go
@@ -0,0 +1,51 @@
+package validator
+
+import "regexp"
+
+type Validator struct {
+	Errors map[string]string
+}
+
+func New() *Validator {
+	return &Validator{make(map[string]string)}
+}
+
+func (v *Validator) AddError(key, errorMessage string) {
+	if _, exists := v.Errors[key]; !exists {
+		v.Errors[key] = errorMessage
+	}
+}
+
+func (v *Validator) Valid() bool {
+	return len(v.Errors) == 0
+}
+
+func (v *Validator) Check(ok bool, key, message string) {
+	if !ok {
+		v.AddError(key, message)
+	}
+}
+
+func Matches(value string, rx *regexp.Regexp) bool {
+	return rx.MatchString(value)
+}
+
+func Unique(values []string) bool {
+
+	uniqueVals := make(map[string]bool)
+
+	for _, val := range values {
+		uniqueVals[val] = true
+	}
+
+	return len(values) == len(uniqueVals)
+}
+
+func In(value string, list ...string) bool {
+	for i := range list {
+		if value == list[i] {
+			return true
+		}
+	}
+	return false
+}
